a12c1092375450d2e2914960b709198c3c47f639
Markus Kannisto <m.kannisto01@gmail.com>
2022-08-18 13:34:35 +0300

    Add test project

---
+++ TestNuGetPackage/TestNuGetPackage.cs
@@ -0,0 +1,12 @@
+﻿namespace Marakusa.TestNuGetPackage
+{
+    public static class TestNuGetPackage
+    {
+        public static float Clamp(float value, float min, float max)
+        {
+            if (value < min)
+                return min;
+            return value > max ? max : value;
+        }
+    }
+}
