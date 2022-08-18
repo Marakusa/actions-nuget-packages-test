cbd1add24a7ea3fcddde3b9c7c50f04bbf5aee1f
Markus Kannisto <m.kannisto01@gmail.com>
2022-08-18 13:38:45 +0300

    Update TestNuGetPackage.cs

--- TestNuGetPackage/TestNuGetPackage.cs
+++ TestNuGetPackage/TestNuGetPackage.cs
@@ -1,12 +1,11 @@
-﻿namespace Marakusa.TestNuGetPackage
+﻿namespace Marakusa.TestNuGetPackage;
+
+public static class TestNuGetPackage
 {
-    public static class TestNuGetPackage
+    public static float Clamp(float value, float min, float max)
     {
-        public static float Clamp(float value, float min, float max)
-        {
-            if (value < min)
-                return min;
-            return value > max ? max : value;
-        }
+        if (value < min)
+            return min;
+        return value > max ? max : value;
     }
 }
